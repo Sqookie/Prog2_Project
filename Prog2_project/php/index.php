0f2d70d876733fcf6628ec73935ba409235d8684
Sqookie <71040681+Sqookie@users.noreply.github.com>
2021-09-08 17:57:52 +0200

    Add files via upload

---
+++ Prog2_project/php/index.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+?>
+
+<?php
+	error_reporting(E_ALL ^ E_NOTICE);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="plug-in/bootstrap/css/bootstrap.min.css">
+    <title>Soul Knight</title>
+</head>
+<body>
+    
+</body>
+</html>
