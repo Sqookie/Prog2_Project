65365b5a0dc82c9a3b61030aca80cf0ac132812a
Sqookie <71040681+Sqookie@users.noreply.github.com>
2021-09-13 18:34:04 +0200

    Add files via upload

--- Prog2_project/weapons.php
+++ Prog2_project/weapons.php
@@ -1,5 +1,7 @@
 <?php
     include "config.php";
+
+    $curr_lang = $_SESSION['lang'];
 ?>
 
 <!doctype html>
@@ -17,7 +19,7 @@
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand" href="index.php?lang=<?php echo $curr_lang ?>">
                 <img src="img/logo.jpg" alt="Logo" style="width:45px;">
             </a>    
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,15 +28,15 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link bold pad_right <?php if(basename(__FILE__) == "index.php") {echo "active";} ?>" aria-current="page" href="index.php"><?php echo $lang['home'] ?></a>
+                        <a class="nav-link bold pad_right <?php if(basename(__FILE__) == "index.php") {echo "active";} ?>" aria-current="page" href="index.php?lang=<?php echo $curr_lang ?>"><?php echo $lang['home'] ?></a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link bold pad_right <?php if(basename(__FILE__) == "weapons.php") {echo "active";} ?>"" href="weapons.php"><?php echo $lang['weapon'] ?></a>
+                        <a class="nav-link bold pad_right <?php if(basename(__FILE__) == "weapons.php") {echo "active";} ?>"" href="weapons.php?lang=<?php echo $curr_lang ?>"><?php echo $lang['weapon'] ?></a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link bold pad_right <?php if(basename(__FILE__) == "character.php") {echo "active";} ?>"" href="character.php"><?php echo $lang['character'] ?></a>
+                        <a class="nav-link bold pad_right <?php if(basename(__FILE__) == "character.php") {echo "active";} ?>"" href="character.php?lang=<?php echo $curr_lang ?>"><?php echo $lang['character'] ?></a>
                     </li>
 
                     <li class="nav-item dropdown">
@@ -42,9 +44,9 @@
                             <?php echo $lang['language'] ?>
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item <?php if($_GET['lang'] == "en") {echo "item_active";} ?>" href="weapons.php?lang=en"><?php echo $lang['lang_en'] ?></a></li>
-                            <li><a class="dropdown-item <?php if($_GET['lang'] == "hu") {echo "item_active";} ?>" href="weapons.php?lang=hu"><?php echo $lang['lang_hu'] ?></a></li>
-                            <li><a class="dropdown-item <?php if($_GET['lang'] == "cn") {echo "item_active";} ?>" href="weapons.php?lang=cn"><?php echo $lang['lang_cn'] ?></a></li>
+                            <li><a class="dropdown-item <?php if($curr_lang == "en") {echo "item_active";} ?>" href="weapons.php?lang=en"><?php echo $lang['lang_en'] ?></a></li>
+                            <li><a class="dropdown-item <?php if($curr_lang == "hu") {echo "item_active";} ?>" href="weapons.php?lang=hu"><?php echo $lang['lang_hu'] ?></a></li>
+                            <li><a class="dropdown-item <?php if($curr_lang == "cn") {echo "item_active";} ?>" href="weapons.php?lang=cn"><?php echo $lang['lang_cn'] ?></a></li>
                         </ul>
                     </li>
 
