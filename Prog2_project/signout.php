3d8eecb7d83796c774f58681ed4a9238826bd13a
Sqookie <71040681+Sqookie@users.noreply.github.com>
2021-11-14 23:23:42 +0100

    Add files via upload

--- Prog2_project/signout.php
+++ Prog2_project/signout.php
@@ -2,7 +2,7 @@
 
 	session_unset();
 	session_destroy();
-
-	echo '<meta http-equiv="refresh" content="0; url=index.php?page=home&lang="'.$curr_lang.'">';
+	
+	die(header("Refresh: 0, index.php?page=home&lang=$curr_lang"));
 
 ?>
