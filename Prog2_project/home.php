9f8387e3beb288bb5420814f72b12f1563311986
Sqookie <71040681+Sqookie@users.noreply.github.com>
2021-11-20 14:32:50 +0100

    Add files via upload

--- Prog2_project/home.php
+++ Prog2_project/home.php
@@ -1,7 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css">
 
-<!-- ========== HOME HEADER ========== -->
+<!-- ===================== HOME HEADER ===================== -->
 <?php echo $lang['home_page'] ?>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
